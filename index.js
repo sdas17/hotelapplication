863f1895c30a4ceac5111d42603f63c3e70cdb68
sdas1 <sdas1@miraclesoft.com>
2024-10-03 17:38:48 +0530

    add-dumydata

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const passport = require('./router/auth.js');
 
 dotenv.config();
 
 let app = express();
 
+//middleware
 app.use(bodyParser.json());
 
+// Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next(); // Move on to the next phase
+}
+app.use(logRequest);
+app.use(passport.initialize());
+
+//middleware 
+
+const localAuthMiddleware = passport.authenticate('local', { session: false })
+
+app.get("/", localAuthMiddleware, (req, res) => {
+    res.send("welcome")
+
+})
 
 const personRoutes = require('./router/Person');
 const menuItem = require('./router/MenuItem');
 
 app.use('/person', personRoutes);
-app.use('/menuItem', menuItem);
+app.use('/menuItem', localAuthMiddleware, menuItem);
 
 
 // Start the server
--- model/Person.js
+++ model/Person.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const personalbar = new mongoose.Schema({
     name: {
         type: String,
@@ -25,8 +27,46 @@ const personalbar = new mongoose.Schema({
     }, salary: {
         type: Number,
         require: true
+    }, username: {
+        required: true,
+        type: String
+    },
+    password: {
+        required: true,
+        type: String
+    }
+
+})
+
+personalbar.pre('save', async function (next) {
+    const person = this;
+
+    // Hash the password only if it has been modified (or is new)
+    if (!person.isModified('password')) return next();
+
+    try {
+        // hash password generation
+        const salt = await bcrypt.genSalt(10);
+
+        // hash password
+        const hashedPassword = await bcrypt.hash(person.password, salt);
+
+        // Override the plain password with the hashed one
+        person.password = hashedPassword;
+        next();
+    } catch (err) {
+        return next(err);
     }
 })
+personalbar.methods.comparePassword = async function(candidatePassword){
+    try{
+        // Use bcrypt to compare the provided password with the hashed password
+        const isMatch = await bcrypt.compare(candidatePassword, this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
 
 //create perosonal model
 const Person = mongoose.model("person", personalbar)
